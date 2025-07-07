7d16d15517a6009723d25c2a14378965235763a1
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-07-07 16:44:54 +0900

    Java Day06 종합예제3

--- src/종합/과제2/종합과제2.java
+++ src/종합/과제2/종합과제2.java
@@ -2,6 +2,6 @@ package 종합.과제2;
 
 public class 종합과제2 { // 종합과제2 class start
     public static void main(String[] args) { // main start
-        Waiting[] waitings = new Waiting[100]
+        Waiting[] waitings = new Waiting[100];
     } // main end
 } // 종합과제2 class end
--- src/종합/예제3/BoardService3.java
+++ src/종합/예제3/BoardService3.java
@@ -6,9 +6,6 @@ public class BoardService3 { // BoardService3 class start
     public static void main(String[] args) { // main start
         // Board[] 배열 변수 선언 -> 다양한 속성을 하나의 클래스로 설계하자.
         Board[] boards = new Board[100];        // boards 변수에 Board객체 100개를 저장 가능한 배열 자료 생성
-        for ( int i = 0; i < boards.length; i++){
-            boards[i] = new Board();            // i번째 boards 초기화
-        }
         for ( ; ; ){ // 무한루프 start
             // Scanner를 객체에 저장
             Scanner scan = new Scanner(System.in);
@@ -22,25 +19,28 @@ public class BoardService3 { // BoardService3 class start
                 // 내용과 작성자를 입력받기
                 System.out.print("내용 : ");  String content = scan.next();
                 System.out.print("작성자 : "); String writer = scan.next();
+                // 객체를 생성하여 멤버변수에 입력받은 값 대입
+                Board board = new Board();
+                board.content = content;
+                board.writer = writer;
                 // 빈 공간 찾기
                 boolean check = false;      // false : 출력 실패, true : 출력 성공
                 for ( int i = 0; i < boards.length; i++){   // boards 배열 순회
-                    if ( boards[i].content == null ){       // i번째 boards의 content가 null이라면
-                        boards[i].content = content;
-                        boards[i].writer = writer;          // 비어있는 곳에 입력받은 값을 저장하고
-                        check = true;                       // 체크 바꾸기
+                    if ( boards[i] == null ){       // i번째 boards의 content가 null이라면
+                        boards[i] = board;          // 해당 i번째 boards에 생성한 객체를 대입하기
+                        check = true;               // 체크 바꾸기
                         break;
                     }
                 }
-                if ( check ){
+                if ( check ){       // check가 true면
                     System.out.println("[안내] 글쓰기 성공");
-                }else {
+                }else {             // check가 false면
                     System.out.println("[경고] 게시물을 등록할 공간이 부족합니다.");
                 }
             }else if ( option == 2 ){   // 2를 선택하면
                 System.out.println("============= 게시물 목록 =============");   // 기본 출력문 출력
                 for( int i = 0; i < boards.length; i++){    // boards 순회
-                    if ( boards[i].content != null ){       // i번째 boards의 content가 null이 아니면
+                    if ( boards[i] != null ){               // i번째 boards가 null이 아니면
                         // 작성자 및 내용 출력
                         System.out.printf("작성자 : %s\n", boards[i].writer);
                         System.out.printf("내용 : %s\n", boards[i].content);
