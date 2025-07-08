f93b1ffed6720a2b5d02c03a85d65b06c0bf589b
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-07-08 15:04:31 +0900

    Java Day07 종합예제4

--- src/종합/예제4/BoardController.java
+++ src/종합/예제4/BoardController.java
@@ -1,8 +1,10 @@
 package 종합.예제4;
 
 public class BoardController { // BoardController class start
+    // 1. 멤버변수 선언
     Board[] boards = new Board[100];                // Board 객체 100개를 저장할 수 있는 배열
-    // doPost 메소드 -> 매개변수 o, 반환값 o -> boolean (매개변수)
+
+    // 2. doPost 메소드 -> 매개변수 o, 반환값 o -> boolean 메소드명(매개변수){ }
     boolean doPost(String content, String writer){  // doPost 메소드 start
         Board board = new Board();                  // Board 객체를 생성하고
         // 매개변수로 받은 값들을 객체의 멤버 변수에 저장한다.
@@ -12,13 +14,18 @@ public class BoardController { // BoardController class start
         for ( int i = 0; i < boards.length; i++){   // boards 배열을 순회하여
             if ( boards[i] == null ){               // 빈 공간을 찾으면
                 boards[i] = board;                  // 생성된 객체를 저장한다.
-                check = true;
+                check = true;                       // 저장성공으로 바꾸고
+                break;                              // 반복문 종료
             }
         }
-        if ( check ){ return true; }
-        else { return false; }
+        if ( check ){ return true; }                // 저장성공하면 true 반환
+        else { return false; }                      // 저장실패하면 false 반환
+        // -> 여기서는 그냥 if 안에서 return true;하고 밖에서 return false; 해도 된다!!!!
+        // -> 왜냐면 return은 그대로 함수 종료이기 때문에, 저장성공하면 return true하고 종료되고
+        // -> boards를 다 순회해도 저장을 못하면 그대로 밖에 있는 return false가 실행되기 때문이다
     } // doPost 메소드 end
-    // doGet 메소드 -> 매개변수 x, 반환값 o -> Board[] ( )
+
+    // 3. doGet 메소드 -> 매개변수 x, 반환값 o -> Board[] 메소드명( ){ }
     Board[] doGet(){ // doGet 메소드 start
         return boards;
     } // doGet 메소드 end
--- src/종합/예제4/BoardService4.java
+++ src/종합/예제4/BoardService4.java
@@ -20,20 +20,25 @@ public class BoardService4 { // BoardService4 class start
                 // 내용과 작성자 입력받기
                 System.out.print("내용 : ");      String content = scan.next();
                 System.out.print("작성자 : ");     String writer = scan.next();
-                bc.doPost( content, writer );           // doPost() 메소드 호출하면서, 입력받은 값을 전달
-                if (bc.doPost( content, writer )){          // true면
+                // doPost() 메소드 호출하면서, 입력받은 값을 전달
+                boolean result = bc.doPost( content, writer );
+                if ( result ){                              // true면
                     System.out.println("[안내] 등록 성공");   // 등록 성공
                 }else {                                     // false면
                     System.out.println("[경고] 등록 실패");   // 등록 실패
                 }
             }else if ( option == 2 ){   // 2를 선택했을 때
-                Board[] boards = bc.doGet();            // doGet() 메소드를 호출하면서, Board 배열 반환받기
+                Board[] boards = bc.doGet();                // doGet() 메소드를 호출하면서, boards 배열 반환받기
+                System.out.println("============= 게시물 목록 =============");
                 for ( int i = 0; i < boards.length; i++ ){  // 반환받은 배열을 순회하면서
-
-                }
-
+                    if ( boards[i] != null ){               // null이 아닌 요소를 발견하면
+                        System.out.printf("작성자 : %s\n", boards[i].writer);
+                        System.out.printf("내용 : %s\n", boards[i].content);
+                        System.out.println("------------------------------------");
+                    } // if end
+                } // for end
             }else { // 유효성 검사
-                System.out.println("잘못된 선택입니다. 다시 선택해주세요.");
+                System.out.println("[경고] 잘못된 선택입니다. 다시 선택해주세요.");
             }
         } // 무한루프 end
 
