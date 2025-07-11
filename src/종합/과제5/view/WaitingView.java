e4bc8e29c92465183fb7d26fdf1d7288d8c78017
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-07-11 20:14:42 +0900

    Day10 종합과제5 메소드 추가

--- src/종합/과제5/view/WaitingView.java
+++ src/종합/과제5/view/WaitingView.java
@@ -28,28 +28,34 @@ public class WaitingView {
             System.out.println("======================================");
             System.out.print("입력 : ");      int option = scan.nextInt();
             if ( option == 1 ){
-                System.out.print("전화번호 : ");        String phone = scan.next();
-                System.out.print("인원수 : ");          int count = scan.nextInt();
-                // 입력받은 값을 controller에게 전달 후 결과 저장
-                boolean result = controller.waitingRegis( phone, count);
-                if ( result ){
-                    System.out.println("[안내] 대기 등록이 완료되었습니다.");
-                }else {
-                    System.out.println("[경고] 대기 등록에 실패하였습니다.");
-                }
+                option1();
             }else if ( option == 2 ){
-                System.out.println("============== 대기 현황 ==============");
-                // controller를 통해 배열 받기
-                WaitingDto[] waitings = controller.waitingPrint();
-                for ( int i = 0; i < waitings.length; i++){
-                    if ( waitings[i] != null ){
-                        System.out.printf("%d. 연락처 : %s - 인원 : %d명\n",  i + 1, waitings[i].getPhone(), waitings[i].getCount() );
-                        System.out.println("------------------------------------");
-                    } // if end
-                } // for end
+                option2();
             } // if end
         } // for end
     } // func end
+    public void option1(){
+        System.out.print("전화번호 : ");        String phone = scan.next();
+        System.out.print("인원수 : ");          int count = scan.nextInt();
+        // 입력받은 값을 controller에게 전달 후 결과 저장
+        boolean result = controller.waitingRegis( phone, count);
+        if ( result ){
+            System.out.println("[안내] 대기 등록이 완료되었습니다.");
+        }else {
+            System.out.println("[경고] 대기 등록에 실패하였습니다.");
+        }
+    }
+    public void option2(){
+        System.out.println("============== 대기 현황 ==============");
+        // controller를 통해 배열 받기
+        WaitingDto[] waitings = controller.waitingPrint();
+        for ( int i = 0; i < waitings.length; i++){
+            if ( waitings[i] != null ){
+                System.out.printf("%d. 연락처 : %s - 인원 : %d명\n",  i + 1, waitings[i].getPhone(), waitings[i].getCount() );
+                System.out.println("------------------------------------");
+            } // if end
+        } // for end
+    }
 
 
 
