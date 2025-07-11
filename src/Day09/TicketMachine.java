a07aa32068a00ba37b21c375bd3b0387d36cc41e
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-07-11 10:21:21 +0900

    Java Day10 1교시

--- src/Day09/TicketMachine.java
+++ src/Day09/TicketMachine.java
@@ -6,12 +6,12 @@ public class TicketMachine {
     // 2. 생성자
 
     // 3. 메소드
-    // 티켓 발권 메소드
+    // 티켓 발권 메소드 ( 일반 메소드 )
     void issueTicket(){
         totalTickets++;
         System.out.println("티켓 1장을 발권했습니다.");
     }
-    // 발권된 티켓수 출력 메소드
+    // 발권된 티켓수 출력 메소드 ( 객체가 필요없는 정적 메소드 )
     static void printTotalTickets(){
         System.out.printf("총 발권 수 : %d장\n", totalTickets);
     }
