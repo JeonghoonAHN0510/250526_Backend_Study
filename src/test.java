8cbb5cdcddf3642b26f494facea69fc7037f99cc
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-07-22 18:07:24 +0900

    JAVA day17

--- src/test.java
+++ src/test.java
@@ -1,11 +1,6 @@
 public class test {
     public static void main(String[] args) {
 
-        long n = 5000;
-        StringBuilder str = new StringBuilder();
-        str.append(n);
-        System.out.println( str );
-
 
     } // main end
 } // class end
