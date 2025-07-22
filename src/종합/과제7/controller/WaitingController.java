8758ac8fd46fb483f9a9c578ed821867cc09ff4b
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-07-22 14:32:46 +0900

    JAVA day17 과제7 진행중

--- src/종합/과제7/controller/WaitingController.java
+++ src/종합/과제7/controller/WaitingController.java
@@ -1,6 +1,9 @@
 package 종합.과제7.controller;
 
 import 종합.과제7.model.dao.WaitingDao;
+import 종합.과제7.model.dto.WaitingDto;
+
+import java.util.ArrayList;
 
 public class WaitingController {
     // 싱글톤 만들기
@@ -19,7 +22,12 @@ public class WaitingController {
     // 매개변수 : String phone, int count
     // 반환값 : boolean
     public boolean waitingRegis( String phone, int count ){
-
+        // 1) 입력받은 값을 dto 객체로 만들기
+        WaitingDto waitingDto = new WaitingDto( phone, count );
+        // 2) 생성한 객체를 dao에게 전달하고, 결과를 받아 view에게 전달
+        boolean result = waitingDao.waitingRegis( waitingDto );
+        // 3) 결과 리턴
+        return result;
     } // func end
 
 
@@ -27,7 +35,9 @@ public class WaitingController {
     // 메소드명 : waitingPrint
     // 매개변수 : X
     // 반환값 : waitingDB -> ArrayList<WaitingDto>
-
-
-
+    public ArrayList<WaitingDto> waitingPrint(){
+        // 1) dao로부터 결과를 받아 view에게 전달
+        ArrayList<WaitingDto> result = waitingDao.waitingPrint();
+        return result;
+    } // func end
 } // class end
--- src/종합/과제7/model/dao/WaitingDao.java
+++ src/종합/과제7/model/dao/WaitingDao.java
@@ -2,6 +2,7 @@ package 종합.과제7.model.dao;
 
 import 종합.과제7.model.dto.WaitingDto;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class WaitingDao {
@@ -32,6 +33,46 @@ public class WaitingDao {
     // 메소드명 : waitingPrint
     // 매개변수 : X
     // 반환값 : waitingDB -> ArrayList<WaitingDto>
+    public ArrayList<WaitingDto> waitingPrint(){
+        // 1) waitingDB 리턴
+        return waitingDB;
+    } // func end
 
+    //==========================================================
+    // CSV 파일 경로 지정
+    private String path = "src/종합/과제7";
+    // [1] CSV 파일 연동 함수
+    public void openCSV(){
+        // 1) 파일 객체 생성
+        File file = new File( path );
+        // 2-1) 파일이 존재하면
+        if ( file.exists() ){
+            // 3-1) 정보 불러오기 -> loadCSV();
+            loadCSV();
+        }else { // 2-2) 파일이 존재하지 않으면
+            // 3-2) 파일 생성 .createNewFile(); -> 일반예외 -> try-catch
+            try {
+                file.createNewFile();
+            } catch ( Exception e ){
+                System.out.println( e );
+            } // try-catch end
+        } // if end
+    } // func end
 
+    // [2] CSV 입력(호출) 함수
+    public void loadCSV(){
+        // 1) CSV 정보를 받아와서
+
+        // 2) CSV를 .readAll(); 하여 List 배열로 바꾸고
+
+        // 3) List 배열을 순회하면서 row 데이터를 객체에 저장하고
+
+        // 4) 생성한 객체를 리스트에 저장
+
+    } // func end
+
+    // [3] CSV 출력(저장) 함수
+    public void saveCSV(){
+
+    } // func end
 } // class end
