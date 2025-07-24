00459d47c320ce8dee1f0d3793afccbc235e525c
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-07-24 16:45:29 +0900

    JAVA DAY19 test

--- src/test.java
+++ src/test.java
@@ -1,43 +1,17 @@
-import java.util.ArrayList;
-import java.util.Objects;
-
 class Solution {
-    public int[] solution(int n, int m) {
-        int[] answer = new int[2];
+    public int solution(String t, String p) {
+        int answer = 0;
 
-        // 최대공약수 구하기
-        ArrayList<Integer> n_max = new ArrayList<>();
-        ArrayList<Integer> m_max = new ArrayList<>();
-        int max = 0;
-        for ( int i = 1; i <= n; i++ ){
-            if ( n % i == 0 ){
-                n_max.add(i);
-            } // if end
+        int tLength = t.length();
+        int pLength = p.length();
+
+        for ( int i = 0; i <= tLength - p.length(); i++ ){
+            int intT = Integer.parseInt( t.substring( i , pLength++ ) );
+            int intP = Integer.parseInt( p );
+            if ( intT <= intP ){
+                answer++;
+            }
         } // for end
-        for ( int i = 1; i <= m; i++ ){
-            if ( m % i == 0 ){
-                m_max.add(i);
-            } // if end
-        } // for end
-        for ( int i = 0; i < n_max.size(); i++ ){
-            System.out.println( n_max.get(i) );
-            for ( int j = 0; j < m_max.size(); j++ ){
-                if (Objects.equals(n_max.get(i), m_max.get(j))){
-                    max = m_max.get(j);
-                    break;
-                } // if end
-            } // for end
-        } // for end
-        System.out.println( "최대공약수 : " + max );
-        answer[0] = max;
-
-        // 최소공배수 구하기
-        int min = n * m / max;
-        answer[1] = min;
-
-
-        System.out.println( "최소공배수 : " + min );
-
         return answer;
     }
 }
@@ -46,8 +20,10 @@ class Solution {
 public class test {
     public static void main(String[] args) {
 
-        new Solution().solution( 3 , 12 );
-        new Solution().solution( 2 , 5 );
+        int answer1 = new Solution().solution("3141592", "271");            System.out.println( answer1 );
+        int answer2 = new Solution().solution("500220839878", "7");         System.out.println( answer2 );
+        int answer3 = new Solution().solution("10203", "15");               System.out.println( answer3 );
+        int answer4 = new Solution().solution("1", "1");                    System.out.println( answer4 );
 
     } // main end
 } // class end
