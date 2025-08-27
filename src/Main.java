d50396be0bd99cddca6416b12e3daa4022c4a20c
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-08-27 21:38:16 +0900

    build[Main] : 백준 기준으로 클래스명 변경

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+
+
+        return answer;
+    } // func end
+} // class end
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] p1 = new int[]{ 70, 50, 80, 50 };
+        int[] p2 = new int[]{ 70, 80, 50 };
+
+
+        int answer1 = new Solution().solution( p1, 100 );
+        int answer2 = new Solution().solution( p2, 100 );
+
+        System.out.println("answer1 = " + answer1);     // 3
+        System.out.println("answer2 = " + answer2);     // 3
+
+    } // main end
+} // class end
