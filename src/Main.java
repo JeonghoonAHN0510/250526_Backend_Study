a7031f782e6f267c6facdeb79a0682fce6b73500
JeonghoonAHN0510 <jeonghoonahn0510@gmail.com>
2025-09-22 18:11:59 +0900

    build[Coding Test] : 백준 코테를 위한 Main.java 기본형

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        StringBuilder answer = new StringBuilder();
 
 
 
+        bw.write(answer.toString().trim());
+        bw.flush();
+        bw.close();
     } // main end
 } // class end
