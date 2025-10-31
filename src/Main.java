9dfae47a77ec36d6aa65765b0b3744587e8f31c9
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-10-31 09:14:12 +0900

    우선순위큐 - 보석 도둑

--- src/Main.java
+++ src/Main.java
@@ -8,66 +8,40 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        int T = Integer.parseInt(br.readLine());        // 테스트케이스 T
-        for (int i = 0; i < T; i++){
-            int M = Integer.parseInt(br.readLine());    // 수열 크기 M
-            answer.append(M / 2 + 1).append("\n");
-            // 큰 우선순위큐와 작은 우선순위큐를 나눠서 생각해보기
-            PriorityQueue<Integer> 중앙값기준작은큐 = new PriorityQueue<>(Collections.reverseOrder());
-            PriorityQueue<Integer> 중앙값기준큰큐 = new PriorityQueue<>();
-            for (int j = 0; j < M / 10 + 1; j++){
-                int count = 1;
-                st = new StringTokenizer(br.readLine());
-                while(st.hasMoreTokens()){
-                    int num = Integer.parseInt(st.nextToken());
-                    // 각 큐의 size를 비교해서 짝수일때는 항상 같은 size를 유지하게
-                    // size가 다르면, 각 peek를 비교해서 넣기
-                    if (중앙값기준작은큐.isEmpty() && 중앙값기준큰큐.isEmpty()){
-                        중앙값기준작은큐.add(num);
-                    } else if (중앙값기준작은큐.isEmpty() && !중앙값기준큰큐.isEmpty()){
-                        if (중앙값기준큰큐.peek() > num){
-                            중앙값기준작은큐.add(num);
-                        } else {
-                            중앙값기준작은큐.add(중앙값기준큰큐.poll());
-                            중앙값기준큰큐.add(num);
-                        } // if end
-                    } else if (!중앙값기준작은큐.isEmpty() && 중앙값기준큰큐.isEmpty()){
-                        if (중앙값기준작은큐.peek() > num){
-                            중앙값기준큰큐.add(중앙값기준작은큐.poll());
-                            중앙값기준작은큐.add(num);
-                        } else {
-                            중앙값기준큰큐.add(num);
-                        } // if end
-                    } else {
-                        if (중앙값기준작은큐.peek() > num){
-                            중앙값기준큰큐.add(중앙값기준작은큐.poll());
-                            중앙값기준작은큐.add(num);
-                        } else if (중앙값기준큰큐.peek() < num){
-                            중앙값기준작은큐.add(중앙값기준큰큐.poll());
-                            중앙값기준큰큐.add(num);
-                        } else {
-                            if (중앙값기준작은큐.size() > 중앙값기준큰큐.size()){
-                                중앙값기준큰큐.add(num);
-                            } else {
-                                중앙값기준작은큐.add(num);
-                            } // if end
-                        } // if end
-                    } // if end
-                    System.out.println("중앙값기준작은큐 = " + 중앙값기준작은큐);
-                    System.out.println("중앙값기준큰큐 = " + 중앙값기준큰큐);
-                    System.out.println("====================");
-                    if (count % 2 == 1){        // 홀수면
-                        if (중앙값기준작은큐.size() > 중앙값기준큰큐.size()){
-                            answer.append(중앙값기준작은큐.peek()).append(" ");
-                        } else {
-                            answer.append(중앙값기준큰큐.peek()).append(" ");
-                        } // if end
-                    } // if end
-                    count++;
-                } // while end
-            } // for end
-            answer.append("\n");
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());       // 보석 개수 N
+        int K = Integer.parseInt(st.nextToken());       // 가방 개수 K
+        PriorityQueue<Map<Integer, List<Integer>>> pq = new PriorityQueue<>();
+
+
+        Map<Integer, Integer> jewelMap = new TreeMap<>();
+        for (int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            jewelMap.put(a, Math.max(jewelMap.getOrDefault(a, 0), b));
         } // for end
+        PriorityQueue<Integer> bagPQ = new PriorityQueue<>();
+        for (int i = 0; i < K; i++){
+            bagPQ.offer(Integer.parseInt(br.readLine()));
+        } // for end
+        long totalWeight = 0;
+        for (int weight : bagPQ){
+            int maxWeight = 0;
+            int maxKey = 0;
+            for (Integer key : jewelMap.keySet()){
+                if (key <= weight){
+                    maxWeight = Math.max(maxWeight, jewelMap.get(key));
+                    maxKey = key;
+                } else {
+                    break;
+                } // if end
+            } // for end
+            totalWeight += maxWeight;
+            jewelMap.remove(maxKey);
+        } // for end
+
+        answer.append(totalWeight);
 
         bw.write(answer.toString().trim());
         bw.flush();
